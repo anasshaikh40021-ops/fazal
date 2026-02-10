6934aaeeb9dfad87aba5d8197823ae4fccd14642
Anas Shaikh <anas.shaikh@example.com>
2026-02-10 17:15:14 +0530

    Add CLOUDINARY SETTING

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,14 +1,22 @@
-import {v2 as cloudinary } from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 
-const connectCloudinary = async () => {
+const connectCloudinary = () => {
+  if (
+    !process.env.CLOUDINARY_CLOUD_NAME ||
+    !process.env.CLOUDINARY_API_KEY ||
+    !process.env.CLOUDINARY_API_SECRET
+  ) {
+    console.error("❌ Cloudinary ENV missing");
+    return;
+  }
 
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key:process.env.CLOUDINARY_API_KEY,
-        api_secret:process.env.CLOUDINARY_SECRET_KEY
-    })
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
-
-}
+  console.log("☁️ Cloudinary connected");
+};
 
 export default connectCloudinary;
